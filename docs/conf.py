598a8bebfe37255c6b7c99f3bcc0d7a64406f82a
Edward Ong <edward.ong@tiliter.com>
2022-07-13 10:35:10 +1000

    fuck pathing

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import pathlib
 import sys
-sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent))
+sys.path.insert(0, str(pathlib.Path(__file__).resolve().parent.parent))
 
 
 # -- Project information -----------------------------------------------------
