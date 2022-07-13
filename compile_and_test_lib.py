ed8df375c53da89f59c9ed34a71b16410f1246e7
Edward Ong <edward.ong@tiliter.com>
2022-07-13 21:59:02 +1000

    fix package

--- compile_and_test_lib.py
+++ compile_and_test_lib.py
@@ -1,58 +1,39 @@
 import pathlib
+import shutil
 import subprocess
+from zipfile import ZipFile
+import urllib.request
 
 
 def execute():
-    """
-        do the equivalent of the following shell script:
-
-        rm -rf build_artifacts/*.*
-        rm -rf gs1-barcode-engine
-
-        git clone https://github.com/gs1/gs1-barcode-engine.git
-        cd gs1-barcode-engine/src/c-lib
-        git checkout 2021-09-10
-        make test
-        make
-        cp build/*.* ../../../build_artifacts
-
-        cd ../../..
-        rm -rf gs1-barcode-engine
-        """
-        
     BUILD_ARTIFACTS_FOLDER_NAME = "build_artifacts"
     LIBRARY_NAME = "gs1-barcode-engine"
-    build_root = pathlib.Path(__file__).resolve().parent
-    source_location = build_root / LIBRARY_NAME / "src" / "c-lib"
-    build_output = source_location / "build"
-    build_artifacts_location = build_root / BUILD_ARTIFACTS_FOLDER_NAME
 
-    _ = subprocess.check_output(["rm", "-rf", LIBRARY_NAME], cwd=build_root)
-    _ = subprocess.check_output(
-        ["rm", "-rf", BUILD_ARTIFACTS_FOLDER_NAME], cwd=build_root
-    )
+    build_root_path = pathlib.Path(__file__).resolve().parent
+    library_path = build_root_path / LIBRARY_NAME
+    library_source_path = library_path / "src" / "c-lib"
+    build_output_path = library_source_path / "build"
+    build_artifacts_path = build_root_path / BUILD_ARTIFACTS_FOLDER_NAME
 
-    _ = subprocess.check_output(
-        ["git", "clone", "https://github.com/gs1/gs1-barcode-engine.git"],
-        cwd=build_root,
-    )
-    _ = subprocess.check_output(["git", "checkout", "2021-09-10"], cwd=source_location)
-    _ = subprocess.check_output(["make", "test"], cwd=source_location)
-    _ = subprocess.check_output(["make"], cwd=source_location)
-    _ = subprocess.check_output(
-        ["rm", "-rf", BUILD_ARTIFACTS_FOLDER_NAME],
-        cwd=build_root,
-    )
-    _ = subprocess.check_output(
-        ["mkdir", BUILD_ARTIFACTS_FOLDER_NAME],
-        cwd=build_root,
-    )
+    shutil.rmtree(build_artifacts_path, ignore_errors=True)
+    shutil.rmtree(library_path, ignore_errors=True)
 
-    _ = subprocess.check_output(
-        [f"cp *.* {str(build_artifacts_location)}"],  # hacky as hell
-        cwd=build_output,
-        shell=True,  # needed for wildcard operator to wokr
+    archive_path = build_root_path / "archive.zip"
+    urllib.request.urlretrieve(
+        "https://github.com/gs1/gs1-barcode-engine/archive/refs/tags/2021-09-10.zip",
+        archive_path,
     )
+    with ZipFile(archive_path, "r") as z:
+        z.extractall(build_root_path)
+    archive_path.unlink()
+
+    shutil.move("gs1-barcode-engine-2021-09-10", LIBRARY_NAME)
+
+    _ = subprocess.check_output(["make", "test"], cwd=library_source_path)
+    _ = subprocess.check_output(["make"], cwd=library_source_path)
+
+    shutil.rmtree(build_artifacts_path, ignore_errors=True)
+    shutil.copytree(build_output_path, build_artifacts_path)
 
 
 if __name__ == "__main__":
